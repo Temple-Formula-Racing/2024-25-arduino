3a10887fc5b37ecb07bd647c3adffff186ae7a62
bdotsamir <samir.js.zip@gmail.com>
2024-10-03 15:12:00 -0400

    updated mcp2515 sketch to include more logging information

--- sketch_oct2a/sketch_oct2a.ino
+++ sketch_oct2a/sketch_oct2a.ino
@@ -15,43 +15,70 @@ void setup() {
 
   mcp2515.reset();
   mcp2515.setBitrate(CAN_1000KBPS, MCP_8MHZ);
-  mcp2515.setNormalMode();
-}
-
-String toHexString(int num) {
-  char hexString[9];  // Enough space for 8 digits + null terminator
-  sprintf(hexString, "%08X", num);  // Converts the number to hex format
-  return String(hexString);  // Returns as a String object
+  mcp2515.setListenOnlyMode();
 }
 
 void loop() {
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    // Serial.println("MESSAGE RECEIVED!");
-    Serial.print("CAN ID: ");
-    Serial.print(toHexString(canMsg.can_id));
-    Serial.print(" data: ");
-    
-    for(short i = 0; i < FRAME_SIZE; i++) {
-      Serial.print(canMsg.data[i]);
-      Serial.print(" ");
+
+    // Standard CAN ID is 11 bits
+    uint32_t standard_can_id = canMsg.can_id & 0x7FF; // Mask for the lower 11 bits
+    // the struct that is exposed from the mcp2515 library encodes the:
+    // 1. can id
+    // 2. (other flags)
+    // and the CAN ID is encoded in the lower 11 bits.
+
+    switch (standard_can_id) {
+      case 0x4ED: { // accel
+        // 1, 2 x angular
+        // 3, 4 y angular
+        // 5, 6 z angular
+        // 7, 8 unused
+        int x = (canMsg.data[0] << 8) | canMsg.data[1];
+        int y = (canMsg.data[2] << 8) | canMsg.data[3];
+        int z = (canMsg.data[4] << 8) | canMsg.data[5];
+
+        Serial.print("ACCEL: ");
+        Serial.print(x);
+        Serial.print(" ");
+        Serial.print(y);
+        Serial.print(" ");
+        Serial.print(z);
+        Serial.println(" ");
+
+        break;
+      }
+
+      case 0x100: { // battery
+
+        int v = (canMsg.data[0] << 8) | canMsg.data[1];
+        float scaled = (float) v * 0.01;
+
+        Serial.print("BATTERY: ");
+        Serial.print(scaled);
+        Serial.println(" volts");
+        delay(2 * 1000);
+        break;
+      }
+
+      default:
+        {
+          Serial.print("Unknown ");
+          Serial.print("CAN ID: ");
+          Serial.print(standard_can_id, HEX);
+
+          // Serial.print(" data: ");
+          Serial.print(" data (");
+          Serial.print(canMsg.can_dlc);
+          Serial.print(" bytes): ");
+
+          for (short i = 0; i < canMsg.can_dlc; i++) {
+            Serial.print(canMsg.data[i]);
+            Serial.print(" ");
+          }
+          Serial.println("");
+          break;
+        }
     }
-    Serial.println("");
-    // if (canMsg.can_id == 0x100)  // Check if the message is from the sender
-    // {
-    //   int battery = (canMsg.data[0] << 8) | canMsg.data[1];  // Combine MSB and LSB
-    //   // //float temperatureC = tempInt / 100.0; // Convert back to float
-
-    //   // Serial.print("Temperature received: ");
-    //   // Serial.print(battery);
-    //   // Serial.println(" °C");
-
-    //   Serial.print("Can data:");
-    //   Serial.println(canMsg.data[0]);
-
-    //   // Send acknowledgment
-    //   // canMsg.can_dlc = 0;           // No data needed for ACK
-    //   // mcp2515.sendMessage(&canMsg);
-    //   // Serial.println("ACK sent");
-    // }
   }
 }
